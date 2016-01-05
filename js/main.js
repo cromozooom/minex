afadbbab65e7fb5f03b81f519bf5ddf19f9b2fa1
cromozooom <cromozooom@gmail.com>
2016-01-06 00:29:27 +0100

    debug mmenu functional - bxslider

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-$(".my-sticky-element").stick_in_parent();
+//$(".my-sticky-element").stick_in_parent();
 
-$('.my-sticky-element')
-.on('sticky_kit:bottom', function(e) {
-    $(this).parent().css('position', 'static');
-})
-.on('sticky_kit:unbottom', function(e) {
-    $(this).parent().css('position', 'relative');
-})
+//$('.my-sticky-element')
+//.on('sticky_kit:bottom', function(e) {
+//    $(this).parent().css('position', 'static');
+//})
+//.on('sticky_kit:unbottom', function(e) {
+//    $(this).parent().css('position', 'relative');
+//})
 
 $(function() {
 	$('nav#menu').mmenu({

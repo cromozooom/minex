8584251f500938a5d0ccd53190bc07c3fa2479a3
cromozooom <cromozooom@gmail.com>
2016-01-05 23:15:28 +0100

    update mmenu

--- js/main.js
+++ js/main.js
@@ -8,13 +8,31 @@ $('.my-sticky-element')
     $(this).parent().css('position', 'relative');
 })
 
-$(document).ready(function() {
-	$("#my-menu").mmenu({
-		// options
-	}, {
-		// configuration
-		offCanvas: {
-			pageNodetype: "section"
-		}
+$(function() {
+	$('nav#menu').mmenu({
+		extensions	: [ 'effect-slide-menu', 'pageshadow' ],
+		searchfield	: true,
+		counters	: true,
+		navbar 		: {
+			title		: 'Advanced menu'
+		},
+		navbars		: [
+			{
+				position	: 'top',
+				content		: [ 'searchfield' ]
+			}, {
+				position	: 'top',
+				content		: [
+					'prev',
+					'title',
+					'close'
+				]
+			}, {
+				position	: 'bottom',
+				content		: [
+					'<a href="http://mmenu.frebsite.nl/wordpress-plugin.html" target="_blank">WordPress plugin</a>'
+				]
+			}
+		]
 	});
 });

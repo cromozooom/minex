c9bd34a671844c30797b86a0f8509eaa993e34ac
cromozooom <cromozooom@gmail.com>
2016-01-06 21:30:28 +0100

    LEFT MENU

--- js/main.js
+++ js/main.js
@@ -11,10 +11,10 @@
 $(function() {
 	$('nav#menu').mmenu({
 		offCanvas: {
-               position  : "right",
+               position  : "left",
                zposition : "front"
             },
-		extensions	: [ 'effect-slide-menu', 'pageshadow' ],
+		extensions	: [ 'effect-slide-menu' ],
 		searchfield	: true,
 		counters	: true,
 		navbar 		: {
@@ -36,4 +36,36 @@ $(function() {
 	});
 });
 
-alert("Ne inmultim ca iepuri huh?");
+/**
+ * Check a href for an anchor. If exists, and in document, scroll to it.
+ * If href argument ommited, assumes context (this) is HTML Element,
+ * which will be the case when invoked by jQuery after an event
+ */
+function scroll_if_anchor(href) {
+    href = typeof(href) == "string" ? href : $(this).attr("href");
+    
+    // You could easily calculate this dynamically if you prefer
+    var fromTop = 0;
+    
+    // If our Href points to a valid, non-empty anchor, and is on the same page (e.g. #foo)
+    // Legacy jQuery and IE7 may have issues: http://stackoverflow.com/q/1593174
+    if(href.indexOf("#") == 0) {
+        var $target = $(href);
+        
+        // Older browser without pushState might flicker here, as they momentarily
+        // jump to the wrong position (IE < 10)
+        if($target.length) {
+            $('html, body').animate({ scrollTop: $target.offset().top - fromTop });
+            if(history && "pushState" in history) {
+                history.pushState({}, document.title, window.location.pathname + href);
+                return false;
+            }
+        }
+    }
+}    
+
+// When our page loads, check to see if it contains and anchor
+scroll_if_anchor(window.location.hash);
+
+// Intercept all anchor clicks
+$("body").on("click", "a", scroll_if_anchor);
